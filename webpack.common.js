af23d4a018d0e52aa012a473348471d4e5209405
Leonidas-I <morningstar8x@outlook.com>
2020-05-14 10:00:28 +0700

    bundle splitting each npm package

--- webpack.common.js
+++ webpack.common.js
@@ -4,16 +4,18 @@ module.exports = {
   },
   optimization: {
     splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/i,
-          name: "vendors",
-          chunks: "all",
-        },
-        commons: {
-          name: "commons",
-          chunks: "initial",
-          minChunks: 2,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace("@", "")}`;
+          },
         },
       },
     },
